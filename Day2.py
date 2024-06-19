45579bf09b7c1d7646d298e6d15e2dfaa142ea7e
Manamnath <manamnathtiwari@gmail.com>
2024-06-19 06:26:56 +0530

    Readme update 1

---
+++ Day2.py
@@ -0,0 +1,7 @@
+# Welcome to Second Day of the Python Programming From Scratch
+
+# Now Today's objective is to solve maximum physics and maths problem using python
+
+# Problem 2 : Write a Program to Compute basic function used in everyday like sinx , cosx,etc
+#               basically create a tool box for mathematician and physicists.
+
